7271862f71a7354049db7319b1735b5ce973ceb1
TheD1TZ <ItsDitz33@gmail.com>
2023-12-04 13:16:02 +0100

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/supabase",
+    "@nuxtjs/tailwindcss",
+    "@nuxt/image",
+    "@nuxtjs/google-fonts",
+  ],
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
